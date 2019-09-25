5a2118d2745036d7eedc97907079a05451f61db1
NvG8956XD02 <31968189+NvG8956XD02@users.noreply.github.com>
2019-09-25 23:01:44 +0200

    Javítva a javított bruh :blush:

--- Bitfarago_Feladatok(ALPHA)/Feladat7/script.js
+++ Bitfarago_Feladatok(ALPHA)/Feladat7/script.js
@@ -7,5 +7,18 @@ function hetedikf() {
     var ermeVastagsag = 0.2;
 
     document.getElementById("eredmeny").innerHTML = Math.floor(szelesseg / ermeMeret) * Math.floor(hossz / ermeMeret) * Math.floor(mag / ermeVastagsag) + "db fér bele";
+}
 
+function load(){
+    background();
+}
+
+function background(){
+    var styleNumber = Math.floor(Math.random()*8)+1;
+
+    var element = document.createElement("link");
+    element.setAttribute("rel", "stylesheet");
+    element.setAttribute("type", "text/css");
+    element.setAttribute("href", "../stylok/style"+styleNumber+".css");
+    document.getElementsByTagName("head")[0].appendChild(element);
 }
